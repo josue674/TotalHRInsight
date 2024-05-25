376523c904610b3ab2d5e2c5db4f26698bab9718
josue674 <josueaguirre644@gmail.com>
2024-05-24 23:42:08 -0600

    cambios

--- TotalHRInsight.DAL/AuthDbContext.cs
+++ TotalHRInsight.DAL/AuthDbContext.cs
@@ -17,8 +17,8 @@ namespace TotalHRInsight.DAL
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //var connectionString = "server=127.0.0.1;database=Prueba;User=root;Password=123456789";
-            //var connectionString = "server=roundhouse.proxy.rlwy.net;port=13730;database=railway;User=root;Password=SATvDqTvIJapCGnqWxNawRJWhYclPVos";
-            var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+            var connectionString = "server=roundhouse.proxy.rlwy.net;port=13730;database=railway;User=root;Password=SATvDqTvIJapCGnqWxNawRJWhYclPVos";
+            //var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
 
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
--- TotalHRInsight/Program.cs
+++ TotalHRInsight/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+var connectionString = builder.Configuration.GetConnectionString("ConnTHRIDB");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
