4c8abc2428e7486b20aa15b7eff0c695d6325b82
josue674 <josueaguirre644@gmail.com>
2024-05-25 13:03:11 -0600

    fr

--- TotalHRInsight/obj/Debug/net8.0/TotalHRInsight.AssemblyInfo.cs
+++ TotalHRInsight/obj/Debug/net8.0/TotalHRInsight.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TotalHRInsight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c083ae272646669b3ad22cb09c0667d0f9401b51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07f79104f0c7471f3d046764b326c2f4c26c2e8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TotalHRInsight")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TotalHRInsight")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
